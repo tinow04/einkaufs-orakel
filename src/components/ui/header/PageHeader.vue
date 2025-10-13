98a073c660fa5b01f137e7ce97feacce38eeef9a
Torben Mueller <uevwm@student.kit.edu>
2025-10-13 22:07:36 +0200

    feat (HomePage): made the HomePage.vue more "vue like", created FixtureCard.vue and PageHeader.vue.

---
+++ src/components/ui/header/PageHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="page-header">
+    <div class="header-inner">
+      <nav class="nav-bar" aria-label="mainNavigation">
+        <div class="nav-left">
+          <RouterLink to="/" class="nav-link">Startseite</RouterLink>
+          <RouterLink to="/termine" class="nav-link">Termine</RouterLink>
+        </div>
+        <div class="nav-right">
+          <RouterLink to="/kontakt" class="nav-link">Kontakt</RouterLink>
+        </div>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
